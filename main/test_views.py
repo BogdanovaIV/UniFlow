73bdd7b3ca469e7e9b0122da7e65eba015eff501
Irina Bogdanova <irinabogdanova261@gmail.com>
2024-10-14 09:12:42 +0000

    Add tests for Contact page and Home page

--- main/test_views.py
+++ main/test_views.py
@@ -17,3 +17,34 @@ class HomePageTest(TestCase):
         """Test if the home page contains specific text."""
         response = self.client.get(reverse('home'))
         self.assertContains(response, 'Medical University')
+
+    def  test_current_year_in_context(self):
+        """Test if the current year is passed to the template context"""
+        response = self.client.get(reverse('home'))
+        self.assertIn('current_year', response.context) 
+
+
+class ContactPageTests(TestCase):
+    
+    def test_contact_page_status_code(self):
+        """Test if the contact page returns a status code of 200"""
+        response = self.client.get(reverse('contact')) 
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_page_template_used(self):
+        """Test if the contact page uses the correct template"""
+        response = self.client.get(reverse('contact'))
+        self.assertTemplateUsed(response, 'main/contact.html')
+
+    def test_contact_page_contains_content(self):
+        """Test if the contact page contains the correct content"""
+        response = self.client.get(reverse('contact'))
+        self.assertContains(response, 'Get in Touch with Medical University')
+        self.assertContains(response, 'Tole Bi Street 94, Almaty 050000')
+        self.assertContains(response, '+1 (111) 123-4567')
+        self.assertContains(response, 'info@medicaluniversity.com')
+
+    def test_google_maps_api_key_in_context(self):
+        """Test if the Google Maps API key is passed to the template context"""
+        response = self.client.get(reverse('contact'))
+        self.assertIn('google_maps_api_key', response.context)
